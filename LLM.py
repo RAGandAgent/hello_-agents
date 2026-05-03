f851b81df1c00b040655e4c74abc13443abb8655
RAGandAgent <huajiqing666@163.com>
2026-05-03 15:47:14 +0800

    第一次提交

---
+++ LLM.py
@@ -0,0 +1,72 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import List, Dict
+
+
+load_dotenv()
+
+
+class HelloAgentsLLM:
+    def __init__(
+        self,
+        model: str = None,
+        api_key: str = None,
+        base_url: str = None,
+        timeout: int = None,
+    ):
+        self.model = model or os.getenv("OPENAI_MODEL")
+        api_key = api_key or os.getenv("OPENAI_API_KEY")
+        base_url = base_url or os.getenv("OPENAI_BASE_URL")
+        timeout = timeout or int(os.getenv("TIMEOUT", 60))
+
+        if not all([self.model, api_key, base_url]):
+            raise ValueError("模型、API密钥和基础URL必须在.env文件中提供")
+        self.client = OpenAI(api_key=api_key, base_url=base_url, timeout=timeout)
+
+    def think(self, messages: List[Dict[str, str]], temperature: float = 1) -> str:
+        """
+        生成模型的思考结果
+        """
+        print(f"正在调用{self.model}模型...")
+
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages,
+                temperature=temperature,
+                stream=True,
+            )
+            print("大语言模型响应成功:")
+            collected_content = []
+            for chunk in response:
+                content = chunk.choices[0].delta.content or ""
+                print(content, end="", flush=True)
+                collected_content.append(content)
+            print()
+            return "".join(collected_content)
+
+        except Exception as e:
+            print(f"调用LLM API时发生错误: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    try:
+        llm = HelloAgentsLLM()
+
+        exampleMessages = [
+            {"role": "system","content": "You are a helpful assistant that writes Python code.",},
+            {"role": "user", "content": "写一个快速排序算法"},
+        ]
+
+        print("---调用LLM模型---")
+        response = llm.think(exampleMessages)
+        if response:
+            print("模型正常响应:")
+            print(response)
+        else:
+            print("模型响应为空")
+
+    except Exception as e:
+        print(f"发生错误: {e}")
---
+++ react.py
@@ -0,0 +1,76 @@
+from LLM import HelloAgentsLLM
+from registerTool import ToolExecutor
+import re
+
+REACT_PROMPT_TEMPLATE = """
+请注意，你是一个有能力调用外部工具的智能助手。
+
+可用工具如下：
+{tools}
+
+请严格按照以下格式进行回应：
+
+Thought: 你的思考过程，用于分析问题、拆解任务和规划下一步行动。
+
+Action: 你决定采取的行动，必须是以下格式之一：
+- `{{tool_name}}[{{tool_input}}]`：调用一个可用工具。
+- `Finish[最终答案]`：当你认为已经获得最终答案时。
+- 当你收集到足够的信息，能够回答用户的最终问题时，你必须在Action:字段后使用 Finish[最终答案] 来输出最终答案。
+
+现在，请开始解决以下问题：
+Question: {question}
+History: {history}
+"""
+
+
+class RrActAgent:
+    def __init__(self, LLM: HelloAgentsLLM, tool_executor: ToolExecutor, max_steps: int = 10) -> None:
+        self.LLM = LLM
+        self.tool_executor = tool_executor
+        self.max_steps = max_steps
+        self.history = []
+
+    def run(self, question: str):
+        """
+        运行智能体来回答一个问题
+        """
+        self.history = []  # 重置历史记录
+        current_step = 0
+
+        while current_step < self.max_steps:
+            current_step += 1
+            print(f"第 {current_step} 步")
+
+            # 格式化提示词
+            tools_description = self.tool_executor.getAvailableTools()
+            history_str = "\n".join(self.history)
+            prompt = REACT_PROMPT_TEMPLATE.format(
+                history=history_str,
+                question=question,
+                tools=tools_description
+            )
+
+            # 调用LLM生成回复
+            messages = [{"role": "user", "content": prompt}]
+            response = self.LLM.think(messages=messages)
+            if not response:
+                print("智能体回复为空，无法继续。")
+                break
+    def _parse_output(self, output: str):
+        """解析LLM的输出，提取Thought和Action。"""
+        thought_matach = re.search(r"Thought:\s*(.+)", output, re.DOTALL)
+
+        action_matach = re.search(r"Action:\s*(.+)", output, re.DOTALL)
+        thought = thought_matach.group(1).strip() if thought_matach else ""
+        action = action_matach.group(1).strip() if action_matach else ""
+        return thought, action
+    
+    def _execute_action(self, action: str):
+        """解析Action字符串，提取工具名称和输入。"""
+        match = re.match(r"(\w+)\[(.*)\]", action, re.DOTALL)
+        if match:
+            return match.group(1), match.group(2)
+        return None, None
+
+    
+    
